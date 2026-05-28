4ac9315c21df5bd1c49574f1516474e97e53f480
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-05-28 10:09:10 +0900

    Initial commit

---
+++ backend/src/main/java/com/example/jreg0/Jreg0Application.java
@@ -0,0 +1,13 @@
+package com.example.jreg0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jreg0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jreg0Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/jreg0/Jreg0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jreg0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jreg0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
