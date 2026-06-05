2e6575d10f350a4f15c1b070accd6fe8d7369ac0
harada.yuya <harada.yuya@wing.sysrdc.com>
2026-06-05 17:01:39 +0900

    マイグレーションファイル変更取り込み

--- backend/src/main/resources/db/migration/V11_1_20260603__create_reservation.sql
+++ backend/src/main/resources/db/migration/V11_1_20260603__create_reservation.sql
@@ -8,10 +8,6 @@ CREATE TABLE reservation(
     payment_method VARCHAR(36),
     payment_status VARCHAR(24),
     fee INT,
-    account_id VARCHAR(36),
-    FOREIGN KEY (seat_id) REFERENCES seat (id),
-    FOREIGN KEY (train_id) REFERENCES train (id),
-    FOREIGN KEY (boarding_station_id) REFERENCES station (id),
-    FOREIGN KEY (Destination_station_id) REFERENCES station (id)
+    account_id VARCHAR(36)
 );
 
