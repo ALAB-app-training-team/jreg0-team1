32fb531ff88febd12b298cf3e9148ea70d72df93
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-06-10 13:58:58 +0900

    予約完了画面に詳細画面へのボタンを設置

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import 'material-symbols';
-import './App.css'
-import TrainSearchResult from '@/features/searches/TrainSearchResult'
-import ReservationComplete from '@/features/reservations/ReservationComplete'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "material-symbols";
+import "./App.css";
+import TrainSearchResult from "@/features/searches/TrainSearchResult";
+import ReservationComplete from "@/features/reservations/ReservationComplete";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TrainSearchResult/>} />
-        <Route path="/reservationComplete" element={<ReservationComplete/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<TrainSearchResult />} />
+          <Route path="/reservationComplete" element={<ReservationComplete />} />
+          <Route path="/details/:id" element={<ReservationDetail />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
-};
+  );
+}
 
 export default App;
--- frontend/src/features/reservations/ReservationComplete.tsx
+++ frontend/src/features/reservations/ReservationComplete.tsx
@@ -1,15 +1,22 @@
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
-function ReservationComplete(){
-    const location = useLocation();
-    const { state } = location;
+function ReservationComplete() {
+  const location = useLocation();
+  const { state } = location;
+  const navigate = useNavigate();
 
-    return (
+  return (
     <div className="flex flex-col items-center">
-        <h1>予約が完了しました</h1>
-        <h3>予約番号：{state.reservationId}</h3>
+      <h1>予約が完了しました</h1>
+      <h3>予約番号：{state.reservationId}</h3>
+      <button
+        className="contained_btn"
+        onClick={() => navigate("/reservation/${state.reservationId}")}
+      >
+        予約情報詳細
+      </button>
     </div>
-    );
+  );
 }
 
 export default ReservationComplete;
