e29fb8b3a3dfb90d0fab41c72f7dd58215905b8e
harada.yuya <harada.yuya@wing.sysrdc.com>
2026-06-11 13:33:59 +0900

    Controller途中

--- backend/src/test/java/com/example/jreg0/Train/TrainControllerTest.java
+++ backend/src/test/java/com/example/jreg0/Train/TrainControllerTest.java
@@ -1,121 +1,100 @@
 package com.example.jreg0.Train;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.example.jreg0.schedule.ScheduleEntity;
+import com.example.jreg0.train.TrainController;
 import com.example.jreg0.train.TrainEntity;
 import com.example.jreg0.train.TrainService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import sample.entity.ExampleEntity;
-import sample.service.ExampleService;
+import tools.jackson.databind.ObjectMapper;
 
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-// WebMvcTestアノテーションで、ExampleControllerのみをテスト対象に限定してWeb層のテストを設定。
-@WebMvcTest(controllers = ExampleController.class)
-class TrainControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-    // MockMvcをDIで取得し、HTTPリクエスト/レスポンスのテストを行う。
+
+@WebMvcTest(TrainController.class)
+public class TrainControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
-    // TrainServiceのモックを作成してDI。モックの動作を設定してテストに利用。
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockitoBean
     private TrainService trainService;
 
+    private Date departureDate;
 
-    @Test
-    void TrainControllerTest_1() throws Exception {
-//        INSERT INTO train(id, train_number, train_name, route_id, train_nickname, formation)
-//        values ('00000000', '0000', 'hayate-0', '00000000','hayate', 12),
-        // モックの設定
-        List<TrainEntity> mockResponse = new ArrayList<TrainEntity>();
+    private TrainEntity trainEntity;
 
-        ScheduleEntity mockSchedule1 = new ScheduleEntity();
-        TrainEntity mockTrain1 = new TrainEntity();
+    // モックの設定
+    List<ScheduleEntity> mockScheduleList = new ArrayList<ScheduleEntity>();
 
-//        INSERT INTO schedule(id, train_id, station_id, departure_time, arrival_time,departure_track,departure_date)
-//        values ('00000000', '00000000', '00000000', '15:00:00', '15:10:00',1,'2026-06-03'),
-        mockSchedule1.setId("00000000");
-        mockSchedule1.setTrain(mockTrain1);
-        mockSchedule1.setStationId("00000000");
-        mockSchedule1.setDepartureTime(Time(15:00:00));
+    ScheduleEntity mockSchedule0 = new ScheduleEntity();
+    ScheduleEntity mockSchedule1 = new ScheduleEntity();
 
-        mockTrain1.setId("00000000");
-        mockTrain1.setTrainNumber("0000");
-        mockTrain1.setTrainName("hayabusa-0");
-        mockTrain1.setRouteId("00000000");
-        mockTrain1.setTrainNickname("はやぶさ");
-        mockTrain1.setFormation(12);
-        mockResponse.setId("00000000");
-        mockResponse.
-        mockResponse.setMessage("Mocked Example " + testId);
+    TrainEntity mockTrain0 = new TrainEntity();
 
-        // モックされたexampleServiceのgetExampleByIdメソッドが呼ばれたとき、mockResponseを返すよう設定。
-        when(exampleService.getExampleById(testId)).thenReturn(mockResponse);
-
-        // テスト実行
-        mockMvc.perform(get("/api/example/{id}", testId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())            // HTTPステータスコード200（OK）を期待。
-                .andExpect(jsonPath("$.id").value(1))  // JSONの"id"フィールドを検証
-                .andExpect(jsonPath("$.message").value("Mocked Example 1")); // レスポンスJSONの"message"フィールドが期待通りであることを検証。
-
-        // Serviceが1回だけ呼ばれたことを検証
-        verify(exampleService, times(1)).getExampleById(testId);
-
-        // Service2回以上呼ばれたことを検証
-        //verify(exampleService, atLeast(2)).getExampleById(testId);
-
-        //一度も呼び出されていない場合の検証：
-        //verifyNoInteractions(exampleService);
-
-        // サービスの特定メソッドが呼び出されていないことを検証
-        //verify(exampleService, times(0)).getExampleById(any());
-
+    @BeforeEach
+    void setup() {
+        departureDate = new Date(2026, 6, 3);
+        //スケジュールのデータセット
+        mockSchedule0.setId("00000000");
+        mockSchedule0.setStationId("00000000");
+        mockSchedule0.setDepartureTime(new Time(15, 00, 00));
+        mockSchedule0.setArrivalTime(new Time(15, 10, 00));
+        mockSchedule0.setDepartureTrack(1);
+        mockSchedule0.setDepartureDate(new Date(2026, 6, 3));
+        mockSchedule1.setId("00000001");
+        mockSchedule1.setStationId("00000001");
+        mockSchedule1.setDepartureTime(new Time(15, 15, 00));
+        mockSchedule1.setArrivalTime(new Time(15, 25, 00));
+        mockSchedule1.setDepartureTrack(1);
+        mockSchedule1.setDepartureDate(new Date(2026, 6, 3));
+        //列車のデータセット
+        mockTrain0.setId("00000000");
+        mockTrain0.setTrainNumber("0000");
+        mockTrain0.setTrainName("hayabusa-0");
+        mockTrain0.setRouteId("00000000");
+        mockTrain0.setTrainNickname("はやぶさ");
+        mockTrain0.setFormation(12);
+        mockSchedule0.setTrain(mockTrain0);
+        mockSchedule1.setTrain(mockTrain0);
+        mockScheduleList.add(mockSchedule0);
+        mockScheduleList.add(mockSchedule1);
+        mockTrain0.setSchedules(mockScheduleList);
     }
-
+    /**
+     * 出発駅・到着駅・出発日に一致する列車を返却する*
+     */
     @Test
-    void convertToTrainResponseDtoTest_1() throws Exception {
+    void getTrainByStationTest_NomalCase1() throws Exception {
+        //列車取得のサービスモック定義
+        when(trainService.getTrainByStation("00000000","00000001",any(Date.class))).thenReturn(List.of(mockTrain0));
 
-        // モックの設定
-        Long testId = 1L;
-        ExampleEntity mockResponse = new ExampleEntity();
-        mockResponse.setId(testId);
-        mockResponse.setMessage("Mocked Example " + testId);
-
-        // モックされたexampleServiceのgetExampleByIdメソッドが呼ばれたとき、mockResponseを返すよう設定。
-        when(exampleService.getExampleById(testId)).thenReturn(mockResponse);
-
-        // テスト実行
-        mockMvc.perform(get("/api/example/{id}", testId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())            // HTTPステータスコード200（OK）を期待。
-                .andExpect(jsonPath("$.id").value(1))  // JSONの"id"フィールドを検証
-                .andExpect(jsonPath("$.message").value("Mocked Example 1")); // レスポンスJSONの"message"フィールドが期待通りであることを検証。
-
-        // Serviceが1回だけ呼ばれたことを検証
-        verify(exampleService, times(1)).getExampleById(testId);
-
-        // Service2回以上呼ばれたことを検証
-        //verify(exampleService, atLeast(2)).getExampleById(testId);
-
-        //一度も呼び出されていない場合の検証：
-        //verifyNoInteractions(exampleService);
-
-        // サービスの特定メソッドが呼び出されていないことを検証
-        //verify(exampleService, times(0)).getExampleById(any());
-
+        mockMvc.perform(get("/trains").param("start","00000000").param("end","00000001").param("date","2026-06-03")).andExpect(status().isOk());
+        verify(trainService.getTrainByStation("00000000","00000001",departureDate));
+    }
+    /**
+     * 出発駅・到着駅・出発日に一致する列車を返却する*
+     */
+    @Test
+    void getTrainByStationTest_ErrorCase1() throws Exception {
+        mockMvc.perform(get("/trains").param("start","00000000").param("end","00000001")).andExpect(status().isBadRequest());
     }
 
 }
