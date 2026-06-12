d8dfd73f00c2dbd6c7d78c5bdf6d72612ac2d308
harada.yuya <harada.yuya@wing.sysrdc.com>
2026-06-12 15:29:34 +0900

    不要なimport削除

--- backend/src/test/java/com/example/jreg0/Reservation/ReservationControllerTest.java
+++ backend/src/test/java/com/example/jreg0/Reservation/ReservationControllerTest.java
@@ -24,7 +24,6 @@ import java.util.UUID;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
--- backend/src/test/java/com/example/jreg0/Reservation/ReservationServiceTest.java
+++ backend/src/test/java/com/example/jreg0/Reservation/ReservationServiceTest.java
@@ -20,10 +20,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
 
 import java.util.*;
-import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
