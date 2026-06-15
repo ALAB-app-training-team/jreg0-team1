595bab0cc4e58027f09b296bc3aceaec7a2d302a
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-06-15 14:48:17 +0900

    フォーマッタを使用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import '@/App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ReservationInfo from '@/features/details/ReservationInfo';
+import DeleteAllReservation from '@/features/manages/DeleteAllReservation';
 import ReservationComplete from '@/features/reservations/ReservationComplete';
 import TrainSearchResult from '@/features/searches/TrainSearchResult';
-import DeleteAllReservation from './features/manages/DeleteAllReservation';
 
 function App() {
     return (
@@ -19,7 +19,10 @@ function App() {
                         element={<ReservationComplete />}
                     />
                     <Route path="/details/:id" element={<ReservationInfo />} />
-                    <Route path="/delete-all-reservation" element={<DeleteAllReservation />} />
+                    <Route
+                        path="/delete-all-reservation"
+                        element={<DeleteAllReservation />}
+                    />
                 </Routes>
             </BrowserRouter>
         </>
--- frontend/src/features/manages/DeleteAllReservation.tsx
+++ frontend/src/features/manages/DeleteAllReservation.tsx
@@ -1,42 +1,45 @@
-import ENDPOINT from "@/constants/Endpoint"
-import Error from "@/components/layout/Error";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import Error from '@/components/layout/Error';
+import ENDPOINT from '@/constants/Endpoint';
 
 function DeleteAllReservation() {
     const navigate = useNavigate();
-    const [status, setStatus] = useState("pendding");
+    const [status, setStatus] = useState('pendding');
 
-    const handleDeleteAll = async() => {
+    const handleDeleteAll = async () => {
         try {
-            const response = await fetch(ENDPOINT.RESERVATIONS,{
-                method: "DELETE"
+            const response = await fetch(ENDPOINT.RESERVATIONS, {
+                method: 'DELETE',
             });
-            setStatus(response.ok ? "done" : "error")
+            setStatus(response.ok ? 'done' : 'error');
         } catch {
-            return <Error />
-        }   
-    }
+            return <Error />;
+        }
+    };
 
-    if (status === "done"){
+    if (status === 'done') {
         return (
             <div className="flex flex-col items-center gap-8">
                 <h1>削除しました</h1>
-                <button className="contained_btn" onClick={()=> navigate("/")}>
+                <button className="contained_btn" onClick={() => navigate('/')}>
                     検索画面に戻る
                 </button>
             </div>
-        )
-    } else if(status === "error") {
-        return <Error />
+        );
+    } else if (status === 'error') {
+        return <Error />;
     }
 
     return (
-        <button className="flex contained_btn place-self-center bg-logout hover:bg-logout/50" onClick={()=>handleDeleteAll()}>
+        <button
+            className="contained_btn bg-logout hover:bg-logout/50 flex place-self-center"
+            onClick={() => handleDeleteAll()}
+        >
             予約を全削除
         </button>
-    )
+    );
 }
 
-export default DeleteAllReservation
+export default DeleteAllReservation;
