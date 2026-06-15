a1725cb406b313c754f5bbb634f07c78e23ede62
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-06-15 12:58:49 +0900

    画面の表示処理を修正

--- frontend/src/features/manages/DeleteAllReservation.tsx
+++ frontend/src/features/manages/DeleteAllReservation.tsx
@@ -1,28 +1,35 @@
 import ENDPOINT from "@/constants/Endpoint"
 import Error from "@/components/layout/Error";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 function DeleteAllReservation() {
     const navigate = useNavigate();
+    const [status, setStatus] = useState("pendding");
+
     const handleDeleteAll = async() => {
-        const response = await fetch(ENDPOINT.RESERVATIONS,{
-            method: "DELETE"
-        });
-        if (response.ok){
-            return (
-                <div className="flex place-items-center">
-                    <h1 className="text-center">
-                        削除しました
-                    </h1>
-                    <button className="contained_btn" onClick={()=> navigate("/")}>
-                        検索画面に戻る
-                    </button>
-                </div>
-            )
-        } else {
+        try {
+            const response = await fetch(ENDPOINT.RESERVATIONS,{
+                method: "DELETE"
+            });
+            setStatus(response.ok ? "done" : "error")
+        } catch {
             return <Error />
-        }
+        }   
+    }
+
+    if (status === "done"){
+        return (
+            <div className="flex flex-col items-center gap-8">
+                <h1>削除しました</h1>
+                <button className="contained_btn" onClick={()=> navigate("/")}>
+                    検索画面に戻る
+                </button>
+            </div>
+        )
+    } else if(status === "error") {
+        return <Error />
     }
 
     return (
