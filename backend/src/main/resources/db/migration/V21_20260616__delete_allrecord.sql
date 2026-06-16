6563391cf1976a32c5f92ebde12828a9db02b133
harada.yuya <harada.yuya@wing.sysrdc.com>
2026-06-16 15:47:51 +0900

    テストケース追加（EntityNotFoundExceptionの確認テスト）

---
+++ backend/src/main/resources/db/migration/V21_20260616__delete_allrecord.sql
@@ -0,0 +1,15 @@
+DELETE FROM reservation;
+
+DELETE FROM car;
+
+DELETE FROM seat;
+
+DELETE FROM schedule;
+
+DELETE FROM train;
+
+DELETE FROM stopstation;
+
+DELETE FROM route;
+
+DELETE FROM station;
