655b3ed6e6242a888827e43f328c8c89d9299357
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-06-19 17:36:01 +0900

    処理のみ追加

--- backend/src/main/java/com/example/jreg0/car/CarRepository.java
+++ backend/src/main/java/com/example/jreg0/car/CarRepository.java
@@ -1,9 +1,14 @@
 package com.example.jreg0.car;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface CarRepository extends JpaRepository<CarEntity, String> {
+    @EntityGraph(attributePaths = {"seats"})
+    List<CarEntity> findByTrainId(String trainId);
 }
