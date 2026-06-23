b7d104ae369f82c8003b264ab440914c8bdabb47
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-06-23 17:46:55 +0900

    見た目の土台を実装

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -5,13 +5,26 @@ import logo from '@/assets/logo.png';
 function Header() {
     const navigate = useNavigate();
     return (
-        <header className="border-b-primary/20 flex h-24 items-center border-b">
+        <header className="border-b-primary/20 flex h-24 items-center justify-between border-b p-4">
             <img
                 src={logo}
                 alt="jreg0のロゴ"
                 className="h-20"
                 onClick={() => navigate('/')}
             />
+
+            <div className="flex gap-8">
+                <div className="flex gap-2">
+                    <span className="material-symbols-outlined">search</span>
+                    新幹線をさがす
+                </div>
+                <div className="flex gap-2">
+                    <span className="material-symbols-outlined">
+                        confirmation_number
+                    </span>
+                    予約確認
+                </div>
+            </div>
         </header>
     );
 }
