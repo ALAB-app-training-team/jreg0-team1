830e7cfb879447201afe0fce9011de9f72bccf21
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-06-24 11:04:46 +0900

    遷移するように、そのページにいたらボタンみたいになるように修正

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,9 +1,15 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import logo from '@/assets/logo.png';
 
 function Header() {
     const navigate = useNavigate();
+    const location = useLocation();
+    console.log(location);
+
+    const getButtonClass = (page: string) => {
+        return location.pathname === page ? 'contained_btn' : '';
+    };
     return (
         <header className="border-b-primary/20 flex h-24 items-center justify-between border-b p-4">
             <img
@@ -13,17 +19,27 @@ function Header() {
                 onClick={() => navigate('/')}
             />
 
-            <div className="flex gap-8">
-                <div className="flex gap-2">
+            <div className="flex items-center gap-8">
+                <button
+                    className={`flex gap-2 ${getButtonClass('/')} p-1`}
+                    onClick={() => navigate('/')}
+                >
                     <span className="material-symbols-outlined">search</span>
                     新幹線をさがす
-                </div>
-                <div className="flex gap-2">
+                </button>
+                <button
+                    className={`flex gap-2 ${getButtonClass('/details/019ef75a-a6f0-798b-88cf-b1db9a320103')} p-1`}
+                    onClick={() =>
+                        navigate(
+                            '/details/019ef75a-a6f0-798b-88cf-b1db9a320103',
+                        )
+                    }
+                >
                     <span className="material-symbols-outlined">
                         confirmation_number
                     </span>
                     予約確認
-                </div>
+                </button>
             </div>
         </header>
     );
