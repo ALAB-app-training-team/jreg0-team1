099cc5d0cdc04c21fadc5a48f205acad242b6532
harada.yuya <harada.yuya@wing.sysrdc.com>
2026-06-24 17:11:11 +0900

    デザイン作成（BEからの受取以外完成）

--- frontend/src/features/reservations/ReservationList.tsx
+++ frontend/src/features/reservations/ReservationList.tsx
@@ -1,74 +1,110 @@
+import { useState } from 'react';
+
+import ReservationSelectItem from '@/features/reservations/ReservationSelectItem';
+
 function ReservationList() {
+    const [selectedTab, setSelectedTab] = useState<'active' | 'past'>('active');
+
+    const reservations = [
+        {
+            id: '00000001',
+            reservationDate: '2026-06-20',
+            departureDate: '2026-06-20',
+            seatLocation: '1A',
+            carNumber: 1,
+            seatType: '01',
+            departureStationName: '東京',
+            departureTime: '10:00:00',
+            arrivalStationName: '上野',
+            arrivalTime: '10:05:00',
+            departureTrack: 2,
+            trainName: 'hayabusa-1',
+            trainNickname: 'はやぶさ',
+        },
+        {
+            id: '00000001',
+            reservationDate: '2026-06-20',
+            departureDate: '2026-06-24',
+            seatLocation: '1A',
+            carNumber: 1,
+            seatType: '01',
+            departureStationName: '東京',
+            departureTime: '12:00:00',
+            arrivalStationName: '仙台',
+            arrivalTime: '13:05:00',
+            departureTrack: 4,
+            trainName: 'hayabusa-210',
+            trainNickname: 'はやぶさ',
+        },
+    ];
+
+    //const { data: reservations, error: reservationError } = useSWR<
+    //    ReservationDetail[]
+    //>(ENDPOINT.RESERVATIONS, fetcher);
+
+    const now = new Date();
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+
+    const activeReservations = reservations?.filter((reservation) => {
+        const departureDate = new Date(reservation.departureDate);
+        return departureDate >= now;
+    });
+
+    const pastReservations = reservations?.filter((reservation) => {
+        const departureDate = new Date(reservation.departureDate);
+        return departureDate < now;
+    });
+
+    const filteredReservations =
+        selectedTab === 'active' ? activeReservations : pastReservations;
+
     return (
         <>
             <div className="mx-auto flex max-w-4xl flex-col gap-8 p-4">
                 <h1 className="text-4xl font-bold">予約確認</h1>
-                <div className="bg-primary/5 flex gap-6 rounded-3xl p-2">
+                {now.toString()}
+                <div className="bg-primary/8 flex gap-6 rounded-3xl p-2">
                     <div className="flex w-full items-center">
-                        <div className="flex w-full items-center justify-center gap-2 rounded-3xl bg-white p-1">
+                        <button
+                            onClick={() => setSelectedTab('active')}
+                            className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-3xl px-6 py-2 transition ${
+                                selectedTab === 'active'
+                                    ? 'bg-white font-semibold shadow'
+                                    : ''
+                            } `}
+                        >
                             <span className="material-symbols-outlined">
                                 calendar_today
                             </span>
-                            有効（ハード）
-                        </div>
-                        <div className="flex w-full items-center justify-center gap-2 rounded-3xl bg-white p-1">
+                            有効（{activeReservations?.length}）
+                        </button>
+                        <button
+                            onClick={() => setSelectedTab('past')}
+                            className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-3xl px-6 py-2 transition ${
+                                selectedTab === 'past'
+                                    ? 'bg-white font-semibold shadow'
+                                    : ''
+                            } `}
+                        >
                             <span className="material-symbols-outlined">
                                 group
                             </span>
-                            過去（ハード）
-                        </div>
-                        <div className="flex w-full items-center justify-center gap-2 rounded-3xl bg-white p-1">
-                            <span className="material-symbols-outlined">
-                                confirmation_number
-                            </span>
-                            キャンセル（ハード）
-                        </div>
+                            過去（{pastReservations?.length}）
+                        </button>
                     </div>
                 </div>
-                <div className="border-primary/20 flex flex-col gap-4 rounded-2xl border p-8">
-                    <div className="flex-col">
-                        <div className="flex">
-                            <div className="flex grow-2 items-center gap-2 font-semibold">
-                                <span className="material-symbols-outlined">
-                                    confirmation_number
-                                </span>
-                                <h3>1号 - はやぶさ</h3>
-                            </div>
-                            <div className="bg-primary right-0 flex items-center justify-center rounded-xl px-3 text-sm text-white">
-                                有効
-                            </div>
-                        </div>
-                        <div className="flex py-2">
-                            <h5>東京 → 上野</h5>
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <div className="flex w-full flex-col">
-                            <h5>出発</h5>
-                            <h3 className="font-semibold">
-                                2026年6月10日 10:00
-                            </h3>
-                        </div>
-                        <div className="flex w-full flex-col">
-                            <h5>ホーム</h5>
-                            <h3 className="font-semibold">番線</h3>
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <div className="flex flex-col gap-2 self-start">
-                            <h5>座席</h5>
-                            <div className="border-primary/20 flex flex-none rounded-2xl border p-2">
-                                号車
-                            </div>
-                        </div>
-                    </div>
-                    <button className="contained_btn right-0 flex items-center justify-center gap-2">
-                        <span className="material-symbols-outlined">
-                            qr_code_2
-                        </span>
-                        チケットを表示
-                    </button>
-                </div>
+                {filteredReservations && filteredReservations.length > 0 ? (
+                    filteredReservations.map((reservation) => (
+                        <ReservationSelectItem
+                            key={reservation.id}
+                            details={reservation}
+                        />
+                    ))
+                ) : (
+                    <></>
+                )}
             </div>
         </>
     );
