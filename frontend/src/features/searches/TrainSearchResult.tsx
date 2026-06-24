07b9025d6114d859b3ceb523eabbd89d693ff916
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-06-24 14:48:58 +0900

    ISOで日付がずれてしまう可能性があるためISOを使わないように修正

--- frontend/src/features/searches/TrainSearchResult.tsx
+++ frontend/src/features/searches/TrainSearchResult.tsx
@@ -10,12 +10,12 @@ import type { Station } from '@/features/searches/types/Station';
 import type { Train } from '@/features/searches/types/Train';
 
 function TrainSearchResult() {
-    const [date, setDate] = useState<string>(
-        new Date().toISOString().split('T')[0],
-    );
-    const [searchDate, setSearchDate] = useState<string>(
-        new Date().toISOString().split('T')[0],
-    );
+    const today = new Date();
+    const dateStr = (date: Date) => {
+        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+    };
+    const [date, setDate] = useState<string>(dateStr(today));
+    const [searchDate, setSearchDate] = useState<string>(dateStr(today));
     const [searchDepartureStationId, setSearchDepartureStationId] =
         useState<string>('');
     const [searchArrivalStationId, setSearchArrivalStationId] =
@@ -52,19 +52,18 @@ function TrainSearchResult() {
     );
 
     const maxDate = () => {
-        const today = new Date();
         const date = new Date(
             today.getFullYear(),
             today.getMonth() + 1,
             today.getDate() - 1,
         );
-        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+        return dateStr(date);
     };
 
     const handleNextDateSearch = () => {
         const nextDate = new Date(date);
         nextDate.setDate(nextDate.getDate() + 1);
-        const nextDateToString = nextDate.toISOString().split('T')[0];
+        const nextDateToString = dateStr(nextDate);
         setDate(nextDateToString);
         setSearchDate(nextDateToString);
     };
@@ -197,7 +196,7 @@ function TrainSearchResult() {
                                 value={date}
                                 onChange={handleDateChange}
                                 required
-                                min={new Date().toISOString().split('T')[0]}
+                                min={dateStr(today)}
                                 max={maxDate()}
                             />
 
